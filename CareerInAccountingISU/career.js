07c4edca844c26b3733472cae4332e57941dc60c
3CNan <wunanxi257@gmail.com>
2021-11-19 17:18:50 -0500

    change

--- CareerInAccountingISU/career.js
+++ CareerInAccountingISU/career.js
@@ -1,12 +1,15 @@
 var currentstep = 0, i = 0, if_decide = false;
 var sensitivity = 0, ability_coefficient = 0, age = 15;
 var count, hint = "There's no hint for this part.";
+var hsc = false; csc = false; cp = false; jop = false;
 
 var sensitivityObj = document.getElementById("sensitivity");
 var ability_coefficientObj = document.getElementById("ability_coefficient");
 var ageObj = document.getElementById("age");
 
-window.onload = function() {
+window.onload = freshdata();
+
+function freshdata() {
 	sensitivityObj.innerHTML = "Sensitivity: " + sensitivity;
 	ability_coefficientObj.innerHTML = "Ability coefficient: " + ability_coefficient;
 	ageObj.innerHTML = "Age: " + age;
@@ -23,6 +26,7 @@ clickpartObj.onclick = function() {
 	clicktextObj.innerHTML = "Click here to continue";
 	nextstep();
 	nextdcsn();
+	freshdata();
 }
 hintbtnObj.onclick = function() {
 	var clickObj = document.getElementById("clickaudio");
@@ -31,10 +35,8 @@ hintbtnObj.onclick = function() {
 	boxnumbers();
 	if(hinttxtObj.innerHTML == "Hint") {
 		hinttxtObj.innerHTML = hint;
-		console.log(1)
 	} else if(hinttxtObj.innerHTML != "Hint") {
 		hinttxtObj.innerHTML = "Hint";
-		console.log(2)
 	}
 }
 
@@ -49,16 +51,20 @@ function boxnumbers() {
 			hint = "Mathematics, Business and Accounting can be helpful to increase your sensitivity to numbers and can develop your logical thinking. <br>English can also be important for communication and understanding."
 			break;
 		case 2:
-			count = 3;
+			count = 2 + 3;
+			hint = "There's no hint for this part.";
 			break;
 		case 3:
 			count = 3;
+			hint = "There's no hint for this part.";
 			break;
 		case 4:
 			count = 3;
+			hint = "There's no hint for this part.";
 			break;
 		case 5:
 			count = 3;
+			hint = "There's no hint for this part.";
 			break;
 	}
 }
@@ -78,8 +84,11 @@ function nextdialogue() {
 	for(var i = 0; i < textboxObjs.length; i++) {
 		textboxObjs[i].style.display = "none";
 	}
-	if(count == 500) {
-
+	if(is_choose_all() == true) {
+		currentstep += 2;
+	} else if(is_choose_all() == false) {
+		currentstep += 1;
+		nextstep();
 	} else if(if_decide == false) {
 		currentstep += 1;
 		nextstep();
@@ -90,5 +99,71 @@ function nextdcsn() {
 	var decisionObjs = document.getElementsByClassName("decision");
 	if(currentstep == 1) {
 		decisionObjs[0].style.display = "block";
+	} else {
+		for(var i = 0; i < decisionObjs.length; i++) {
+			decisionObjs[i].style.display = "none";
+		}
+	}
+	hscdcsn();
+}
+
+var hscsObjs = document.getElementsByName("hscs");
+
+function hscdcsn() {
+	var i;
+	if(if_hscdcsn_occur() == true) {
+		if(hscsObjs[0].checked) {
+			sensitivity += 1;
+		}
+		if(hscsObjs[2].checked) {
+			sensitivity += 1;
+		}
+		if(hscsObjs[3].checked) {
+			sensitivity += 1;
+		}
+		if(hscsObjs[6].checked) {
+			sensitivity += 1;
+		}
+		for(var i = 0; i < hscsObjs.length; i++) {
+			hscsObjs[i].checked = false;
+		}
 	}
 }
+function if_hscdcsn_occur() {
+	for(var i = 0; i < hscsObjs.length; i++) {
+		if(hscsObjs[i].checked) {
+			return true;
+		}
+	}
+}
+function is_choose_all() {
+	var a = 0;
+	for(var i = 0; i < hscsObjs.length; i++) {
+		if(hscsObjs[i].checked) {
+			a += 1;
+		}
+		if(a == hscsObjs.length - 1) {
+			console.log(a)
+			return true;
+		}
+	}
+}
+function hscdcsnminus() {
+	if(is_choose_all() == true) {
+		sensitivity -= 2;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
